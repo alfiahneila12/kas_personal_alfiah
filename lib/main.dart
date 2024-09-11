05a50592abe6f2789b4a89a470c0767af1bbd9df
sulamaalfiah <sulamaalfiah@gmail.com>
2024-09-11 19:29:49 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:kas_personal/providers/kas_provider.dart';
+import 'package:kas_personal/router.dart';
+import 'package:kas_personal/theme.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => KasProvider(),
+        ),
+      ],
+      child: const App(),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      theme: theme,
+      routerConfig: router,
+    );
+  }
+}
---
+++ lib/router.dart
@@ -0,0 +1,18 @@
+import 'package:go_router/go_router.dart';
+import 'package:kas_personal/screens/home_screen.dart';
+import 'package:kas_personal/screens/kas_masuk_screen.dart';
+import 'package:kas_personal/screens/kas_keluar_screen.dart';
+import 'package:kas_personal/screens/lihat_semua.dart';
+
+final router = GoRouter(routes: [
+  GoRoute(
+    path: '/',
+    name: 'root',
+    builder: (context, state) => const HomeScreen(),
+    routes: [
+      GoRoute(path: 'kas_masuk', name: 'kas_masuk', builder: (context, state) => const KasMasukScreen(),),
+      GoRoute(path: 'kas_keluar', name: 'kas_keluar', builder: (context, state) => const KasKeluarScreen(),),
+      GoRoute(path: 'lihat_semua', name: 'lihat_semua', builder: (context, state) => const LihatSemua(),),
+    ],
+  ),
+]);
